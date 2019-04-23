861c6eb62525364072068ec52f667e113ea035e3
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2019-04-23 10:50:51 +0200

    Aggregate all the output files

--- bench.py
+++ bench.py
@@ -15,33 +15,34 @@ def compile_exec():
     sys.stderr.write(stderr.decode())
 
 
-class WrongPattern(Exception):
-    pass
-
-
-def run(file_pattern, nb_calls, size, core):
+def run(tmpfile, nb_calls, size, core):
     numactl_str = 'numactl --physcpubind=%d --localalloc' % core
-    try:
-        filename = file_pattern % core
-    except TypeError:
-        raise WrongPattern()
-    cmd = '%s ./%s %s %d %d' % (numactl_str, EXEC_NAME, filename, nb_calls, size)
+    cmd = '%s ./%s %s %d %d %d' % (numactl_str, EXEC_NAME, tmpfile, nb_calls, size, core)
     print(cmd)
     return Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
 
 
-def run_all(file_pattern, nb_calls, size, cores):
-    processes = [run(file_pattern, nb_calls, size, c) for c in cores]
+def run_all(filename, nb_calls, size, cores):
+    compile_exec()
+    assert len(set(cores)) == len(cores)
+    tmpfiles = ['/tmp/bench_%d.csv' % c for c in cores]
+    processes = [run(f, nb_calls, size, c) for f, c in zip(tmpfiles, cores)]
     for proc in processes:
         stdout, stderr = proc.communicate()
         sys.stdout.write(stdout.decode())
         sys.stderr.write(stderr.decode())
+    with open(filename, 'w') as f:
+        f.write('start,end,duration,nb_cycles,core_id\n')
+        for tmp in tmpfiles:
+            with open(tmp) as f2:
+                for line in f2:
+                    f.write(line)
 
 
 def main(args):
     parser = argparse.ArgumentParser(description='Stress test with performance and frequency measurements')
-    parser.add_argument('--output_pattern', type=str, default='/tmp/stress_%d.csv',
-                        help='Output file for the measures, must contain a "%d"')
+    parser.add_argument('--output', type=str, default='/tmp/stress_perf.csv',
+                        help='Output file for the measure')
     parser.add_argument('--nb_calls', type=int, default=100,
                         help='Number of calls to perform')
     parser.add_argument('--size', type=int, default=1000000000,
@@ -49,11 +50,7 @@ def main(args):
     parser.add_argument('--cores', type=int, default=[1], nargs='+',
                         help='Cores on which to run the test')
     args = parser.parse_args(args)
-    compile_exec()
-    try:
-        run_all(args.output_pattern, args.nb_calls, args.size, args.cores)
-    except WrongPattern:
-        parser.error('The file pattern must contain one (and only one) "%d"')
+    run_all(args.output, args.nb_calls, args.size, args.cores)
 
 
 if __name__ == '__main__':
