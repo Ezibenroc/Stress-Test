17e7f45ee7ab868a270473cbea9c2be45007b555
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2019-11-28 10:28:08 +0100

    Apply a mask of given size on the lower bits

--- test_flips.c
+++ test_flips.c
@@ -9,6 +9,58 @@
 
 typedef double number;
 
+/*
+ * Return 0b000...01...1, with n ones at the end.
+ */
+uint64_t __get_mask(unsigned n) {
+    if(n == 0)
+        return 0;
+    assert(n >= 0 && n < 64);
+    uint64_t mask = 1;
+    return (mask << n) - 1;
+}
+
+/*
+ * Return 0b000...01...10...0.
+ * The bits at positions [start, stop] are equal to 1, the others to 0 (start is the lower order).
+ */
+uint64_t get_mask(unsigned start, unsigned stop) {
+    assert(0 <= start && start <= stop && stop < 64);
+    uint64_t ones = __get_mask(stop);
+    uint64_t zeroes = ~__get_mask(start);
+    return ones & zeroes;
+}
+
+/*
+ * Apply the given mask to a double.
+ */
+double apply_mask(double x, uint64_t mask) {
+    uint64_t *tmp = (uint64_t*)&x;
+    (*tmp) |= mask;
+    return *((double*)tmp);
+}
+
+void __print_bits(uint64_t n, unsigned i) {
+    if(i == 64)
+        return;
+    __print_bits(n / 2, i+1);
+    if(n % 2)
+        printf("1");
+    else
+        printf("0");
+}
+
+void print_bits(uint64_t n) {
+    printf("0b");
+    __print_bits(n, 0);
+    printf("\n");
+}
+
+void print_bits_f(double x) {
+    uint64_t *tmp = (uint64_t*)&x;
+    print_bits(*tmp);
+}
+
 void measure_call(FILE *f, unsigned long long inner_loop, long id, number tab[6]) {
     timestamp_t start = get_time();
     for(unsigned long long i = 0; i < inner_loop; i++) {
@@ -54,18 +106,21 @@ int main(int argc, char *argv[]) {
 
     number tab[6];
     unsigned long long val_1, val_2;
+    uint64_t mask = get_mask(0, mask_size);
     // the mantisse of the 1st mask is the bit sequence 0b010101... (i.e. 0x555...)
     // the mantisse of the 2nd mask is the bit sequence 0b101010... (i.e. 0xAAA...)
     // for both masks, the exponent is 0x3EF, to have a floating point number not "too large" nor "too small"
     // the sign is 0 (i.e. positive number)
-    val_1 = 0x3EF5555555555555;
-    val_2 = 0x3EFAAAAAAAAAAAAA;
+    val_1 = 0x3EF5555555555555 | mask;
+    val_2 = 0x3EFAAAAAAAAAAAAA | mask;
     for(int i = 0; i < 3; i++) {
         tab[i] = *((double*)&val_1);
     }
     for(int i = 3; i < 6; i++) {
         tab[i] = *((double*)&val_2);
     }
+    print_bits_f(tab[0]);
+    print_bits_f(tab[3]);
 
     printf("%e %e\n", tab[0], tab[3]);
     for(unsigned long long i = 0; i < outer_loop; i++) {
