9eb05a023aaed429793bd381d99aa82c27a2aa2a
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2019-11-28 10:23:08 +0100

    Only keep the adversarial mode

--- test_flips.c
+++ test_flips.c
@@ -38,7 +38,7 @@ void measure_call(FILE *f, unsigned long long inner_loop, long id, number tab[6]
 
 int main(int argc, char *argv[]) {
     if(argc != 6) {
-        fprintf(stderr, "Syntax: %s <filename> <mode> <outer_loop> <inner_loop> <ID>\n", argv[0]);
+        fprintf(stderr, "Syntax: %s <filename> <mask_size> <outer_loop> <inner_loop> <ID>\n", argv[0]);
         exit(1);
     }
     char *end;
@@ -46,42 +46,25 @@ int main(int argc, char *argv[]) {
     unsigned long long inner_loop = strtoull(argv[4], &end, 10);
     long id = strtol(argv[5], &end, 10);
     FILE *f = fopen(argv[1], "w");
-    char *mode = argv[2];
+    unsigned mask_size = strtoul(argv[2], &end, 10);
     if(!f) {
         perror("fopen");
         exit(1);
     }
 
     number tab[6];
-    if(strcmp("random", mode) == 0) {
-        for(int i = 0; i < 6; i++) {
-            tab[i] = (double)rand()/(double)(RAND_MAX);
-        }
+    unsigned long long val_1, val_2;
+    // the mantisse of the 1st mask is the bit sequence 0b010101... (i.e. 0x555...)
+    // the mantisse of the 2nd mask is the bit sequence 0b101010... (i.e. 0xAAA...)
+    // for both masks, the exponent is 0x3EF, to have a floating point number not "too large" nor "too small"
+    // the sign is 0 (i.e. positive number)
+    val_1 = 0x3EF5555555555555;
+    val_2 = 0x3EFAAAAAAAAAAAAA;
+    for(int i = 0; i < 3; i++) {
+        tab[i] = *((double*)&val_1);
     }
-    else if(strcmp("equal", mode) == 0) {
-        double value = (double)rand()/(double)(RAND_MAX);
-        for(int i = 0; i < 6; i++) {
-            tab[i] = value;
-        }
-    }
-    else if(strcmp("adversarial", mode) == 0) {
-        unsigned long long mask_1, mask_2;
-        // the mantisse of the 1st mask is the bit sequence 0b010101... (i.e. 0x555...)
-        // the mantisse of the 2nd mask is the bit sequence 0b101010... (i.e. 0xAAA...)
-        // for both masks, the exponent is 0x3EF, to have a floating point number not "too large" nor "too small"
-        // the sign is 0 (i.e. positive number)
-        mask_1 = 0x3EF5555555555555;
-        mask_2 = 0x3EFAAAAAAAAAAAAA;
-        for(int i = 0; i < 3; i++) {
-            tab[i] = *((double*)&mask_1);
-        }
-        for(int i = 3; i < 6; i++) {
-            tab[i] = *((double*)&mask_2);
-        }
-    }
-    else {
-        fprintf(stderr, "Error, unknown mode '%s', must be 'random', 'equal' or 'adversarial'\n", mode);
-        exit(1);
+    for(int i = 3; i < 6; i++) {
+        tab[i] = *((double*)&val_2);
     }
 
     printf("%e %e\n", tab[0], tab[3]);
